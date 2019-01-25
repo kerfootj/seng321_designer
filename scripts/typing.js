55b2372f58cbd55e174ef2a6829f0d1fd121970c
kerfootj <joelkerfoot@gmail.com>
2019-01-24 18:31:44 -0800

    moved typing script from html to js folder

---
+++ scripts/typing.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#typed', {
+    strings:["^70Developers^1000", "^70Designers^1000", "^70Engineers^1000", "^70Architects^1000", "^70Product Managers^1000", "^70Testers^1000"],
+    typeSpeed: 40,
+    backSpeed: 50,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: true,
+    cursorChar: '|',
+    autoInsertCss: true,
+});
