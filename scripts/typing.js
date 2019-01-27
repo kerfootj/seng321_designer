c1edcc47cd250a34c8935d1b6ed2619be71d3a12
kerfootj <joelkerfoot@gmail.com>
2019-01-27 13:24:09 -0800

    changed typing time to 1500 from 1000

--- scripts/typing.js
+++ scripts/typing.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#typed', {
-    strings:["^70Developers^1000", "^70Designers^1000", "^70Engineers^1000", "^70Architects^1000", "^70Product Managers^1000", "^70Testers^1000"],
+    strings:["^70Developers^1500", "^70Designers^1500", "^70Engineers^1500", "^70Architects^1500", "^70Product Managers^1500", "^70Testers^1500"],
     typeSpeed: 40,
     backSpeed: 50,
     loop: true,
